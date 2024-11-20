d467511a66e865e0745d7c9474984b59bb33424e
jespermagician <jesper@herlings.de>
2024-11-20 16:41:22 +0100

    Edit and add sql files

--- SQL-Files/InsertDefaultData.sql
+++ SQL-Files/InsertDefaultData.sql
@@ -1,12 +1,16 @@
-ALTER TABLE IF EXISTS Users
-    OWNER to postgres;
-
-INSERT INTO Roles ("RoleID", "Role") VALUES (1, 'Admin');
-INSERT INTO Roles ("RoleID", "Role") VALUES (2, 'Helfer');
-INSERT INTO Roles ("RoleID", "Role") VALUES (3, 'Laeufer');
+INSERT INTO Roles (RoleID, RoleNamedefault) VALUES (1, 'Admin');
+INSERT INTO Roles (RoleID, RoleNamedefault) VALUES (2, 'Helfer');
+INSERT INTO Roles (RoleID, RoleNamedefault) VALUES (3, 'Laeufer');
 
 --Passwort = 1234 (Hash MD5)
 INSERT INTO Users 
-    ("IdUser","Name","LastName","Email","Password_hash","Salt","CreatedAt","RoleID","VerifiedUser") 
+    (UserID, FirstName, LastName, Email, Password_Hash, Salt, CreatedAt, fk_RoleID, Verified) 
 VALUES 
-    (1,'Admin','DoRunAdmin','Replace@mail.com','81dc9bdb52d04dc20036dbd8313ed055','81dc9bdb52d04dc20036dbd8313ed055','20.11.2024',1,true);
+    (1,'Admin','DoRunAdmin','Replace@mail.com','81dc9bdb52d04dc20036dbd8313ed055'::bytea,'81dc9bdb52d04dc20036dbd8313ed055'::bytea,'20.11.2024',1,true);
+
+
+
+INSERT INTO Users 
+    (UserID, FirstName, LastName, Email, Password_Hash, Salt, fk_RoleID) 
+VALUES 
+    (2,'Test','User','test@mail.com', '81dc9bdb52d04dc20036dbd8313ed055'::bytea, '81dc9bdb52d04dc20036dbd8313ed055'::bytea, 1);
