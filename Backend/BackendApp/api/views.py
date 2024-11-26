29c5f847c0ed770b587dbce3b0a6c89d73559947
KeineSchere <125075466+KeineSchere@users.noreply.github.com>
2024-11-26 19:07:58 +0100

    Clean Code
    cleaned code + delete useless files

--- Backend/BackendApp/api/views.py
+++ Backend/BackendApp/api/views.py
@@ -1,16 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny
-from django import forms
+from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Users
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import login, authenticate
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -19,59 +14,31 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
-class RegistrationForm(forms.Form):
-    #Input fields for registration
-    first_name = forms.CharField(label='Your First Name', max_length=100, required=True)
-    last_name = forms.CharField(label='Your Last Name', max_length=100, required=True)
-    email = forms.EmailField(label='E-Mail', required=True)
-    password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(),
-        required=True
-    )
-    repeat_password = forms.CharField(
-        label='Repeat Password',
-        widget=forms.PasswordInput(),
-        required=True
-    )
-
-    # Optionale Methode zur Validierung von Passwortfeldern
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        repeat_password = cleaned_data.get("repeat_password")
-
-        if password and repeat_password and password != repeat_password:
-            raise forms.ValidationError("Passwords do not match!")
 
 #Handels the registration page
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
-        # form = RegistrationForm(request.POST)
-        # if form.is_valid():
-            # Daten auslesen
-            data = json.loads(request.body)
-            first_name = data.get("firstname")#form.cleaned_data['first_name']
-            last_name = data.get("lastname")#form.cleaned_data['last_name']
-            email = data.get("email")#form.cleaned_data['email']
-            password = data.get("password")#form.cleaned_data['password']
+        # Daten auslesen
+        data = json.loads(request.body)
+        first_name = data.get("firstname")
+        last_name = data.get("lastname")
+        email = data.get("email")
+        password = data.get("password")
 
-            try :
-            #Erstelle neuen Benutzer auf der Datenbank
-                print(first_name,last_name,email,password)
-                NewUser = Users.RegisterUser(first_name,last_name,email,password)
+        try:
+        #Erstelle neuen Benutzer auf der Datenbank
+            print(first_name,last_name,email,password)
+            NewUser = Users.RegisterUser(first_name,last_name,email,password)
 
-            except : return JsonResponse({'message': 'Registrierung nicht erfolgreich' + str(NewUser)}, status=401)
-            #Nur für Ausgabe der UserID später ersetzen!
-            NewUser = int(NewUser.iduser)
+        except: 
+        #Bei Fehler return error an Frontend
+            return JsonResponse({'message': 'Registrierung nicht erfolgreich'}, status=401)
+        
+        #Convert Userid
+        NewUser = int(NewUser.iduser)
             
-            return JsonResponse({'message': 'Registrierung erfolgreich' + str(NewUser)}, status=200)
-            
-            #return redirect('success')  # Weiterleitung auf eine Erfolgsseite
-            
-    # return render(request, 'register.html', {'form': form})        
+        return JsonResponse({'message': 'Registrierung erfolgreich' + str(NewUser)}, status=200)
 
 #Login user
 @csrf_exempt
