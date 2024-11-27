2229e2fd7977ee3da6248b9a4b7aa38631b6310a
KeineSchere <125075466+KeineSchere@users.noreply.github.com>
2024-11-27 11:32:17 +0100

    Adding donationsrecords DB
    Adding donationsrecords

--- Backend/BackendApp/api/models.py
+++ Backend/BackendApp/api/models.py
@@ -3,6 +3,8 @@ from datetime import date
 from django.shortcuts import redirect
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import logout
+from django.http import JsonResponse
+import json
 
 # Create your models here.
 class Users(models.Model):
@@ -14,8 +16,9 @@ class Users(models.Model):
     salt = models.BinaryField(null=False)
     createdat = models.DateTimeField(auto_now_add=True, null=False)
     roleid = models.IntegerField(null=False)
+    kilometers = models.IntegerField(null=False)
     verifieduser = models.BooleanField()
- 
+    
     def RegisterUser(first_name,last_name,email,password):
         #1. Set UserID
         #Check if email already exists
@@ -89,8 +92,65 @@ class Users(models.Model):
         except:
             print("Error")
             
-                    
+class donationrecords():
+    donationrecid = models.IntegerField(primary_key=True, null=False)
+    iduser = models.TextField(null=False)
+    firstname = models.TextField(null=False)
+    lastname = models.TextField(null=False)
+    email = models.EmailField(unique=False, null=False)
+    street = models.TextField(null=False)
+    housenr = models.TextField(null=False)
+    postcode = models.TextField(null=False)
+    donation = models.FloatField(null=True)
+    fixedamount = models.BooleanField(null=True)
+    createdat = models.DateTimeField(auto_now_add=True, null=False)
+    verified = models.BooleanField(null=True)
+    
+    def GetUserStats(Userid):
         
+        #Get Userdata for Welcome Screen 
+        UserName = Users.objects.raw("Select firstname, lastname, email From api_users Where iduser = %s", [Userid])
+        
+        for row in UserName:
+            UserFirstname = row.firstname
+            UserLastname = row.lastname
+            UserEmail = row.email
+        
+        #Get donationrecords for the loggedin user
+        UserEntrys = donationrecords.objects.raw("Select * From api_donationrecords Where iduser = %s", [Userid])
+        
+        TotalDonations = 0
+        TotalKilometers = 0
+        #Get Total amount for Donations and Total Kilometers 
+        for row in UserEntrys:
+            #Calculate total Donations 
+            if (row.fixedamount == True):
+                TotalDonations += row.donation
+            else:
+                TotalDonations = TotalDonations + (row.donation * row.kilometers)
+            
+            TotalKilometers += row.kilometers
+        
+        data = [
+            {
+             "UserFirstname": UserFirstname,
+             "UserLastname": UserLastname,
+             "UserEmail": UserEmail,
+             "firstname": obj.firstname, "lastname": obj.lastname, 
+             "email": obj.email, "street": obj.street, 
+             "housenr": obj.housenr,"postcode": obj.postcode, 
+             "donation": obj.donation, "fixedamount": obj.fixedamount, 
+             "createdat": obj.createdat, "verified": obj.verified, 
+             "Kilometers": obj.kilometers, 
+             "TotalDonations": TotalDonations,"TotalKilometers": TotalKilometers}  # Felder anpassen
+            for obj in UserEntrys
+        ]
+
+        return JsonResponse(status=200, data={data})
+            
+def roles():
+    roleid = models.IntegerField(primary_key=True,null=False)
+    rolename = models.TextField(null=False)
 
 def PasswordHashing(password):
     Password_Hash = bytes(1)
@@ -112,5 +172,6 @@ class CustomBackend(BaseBackend):
 
 def user_logout(request):
     #Reset acces 
+    request.session["iduser"] = 0
     request.session["UserIsAuth"] = False
     return redirect('login')
