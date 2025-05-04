9e90dddbedbadb5b3f4a2cd92ba863b6d7d93f4a
Jespermagician <jesper@herlings.de>
2025-05-04 16:45:35 +0200

    there are no mistakes only happy accidents

--- Backend/BackendApp/BackendApp/settings.py
+++ Backend/BackendApp/BackendApp/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
     #        "NAME": BASE_DIR / "db.sqlite3",
     #}
 }
-
+# test
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
